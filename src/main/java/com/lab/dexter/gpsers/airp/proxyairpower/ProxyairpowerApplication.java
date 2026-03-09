a06312492d10f5cf7757c2eeb4b21c6c0ab6396d
Davi Freitas <davicfreitas1@gmail.com>
2026-03-09 10:00:38 -0300

    Init Project

---
+++ src/main/java/com/lab/dexter/gpsers/airp/proxyairpower/ProxyairpowerApplication.java
@@ -0,0 +1,13 @@
+package com.lab.dexter.gpsers.airp.proxyairpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyairpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyairpowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/dexter/gpsers/airp/proxyairpower/ProxyairpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.dexter.gpsers.airp.proxyairpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyairpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
