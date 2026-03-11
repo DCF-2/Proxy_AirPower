3ac25bcc317c66051f335e4818f521c46eeb1cf8
Davi Freitas <davicfreitas1@gmail.com>
2026-03-11 08:17:53 -0300

    fix(ssl): adiciona bypass global para certificados autoassinados do thingsboard

---
+++ src/main/java/com/lab/dexter/gpsers/airp/proxyairpower/config/SslDisableConfig.java
@@ -0,0 +1,37 @@
+package com.lab.dexter.gpsers.airp.proxyairpower.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+@Configuration
+public class SslDisableConfig {
+
+    @PostConstruct
+    public void disableSslVerification() {
+        try {
+            // Cria um gestor de confiança cego (igual ao do seu Android)
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() { return null; }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                    }
+            };
+
+            // Aplica a configuração globalmente
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
