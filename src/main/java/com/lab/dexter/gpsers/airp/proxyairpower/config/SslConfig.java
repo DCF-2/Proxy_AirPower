2af17a27e970f27c0079bb4d6a7b3babcd96baa9
Davi Freitas <davicfreitas1@gmail.com>
2026-04-16 12:59:08 -0300

    chore(dev): implementa bypass de SSL no RestTemplate para ambiente de laboratório

--- src/main/java/com/lab/dexter/gpsers/airp/proxyairpower/config/SslConfig.java
+++ src/main/java/com/lab/dexter/gpsers/airp/proxyairpower/config/SslConfig.java
@@ -3,47 +3,41 @@ package com.lab.dexter.gpsers.airp.proxyairpower.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
 
-// Imports corretos do HttpClient 5
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.TrustStrategy;
 
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
-
-import java.io.File;
 
 @Configuration
 public class SslConfig {
 
-    @Value("${trust.store.path:classpath:truststore.jks}")
-    private String trustStorePath;
-
-    @Value("${trust.store.password:}")
-    private String trustStorePassword;
-
     @Bean
     public RestTemplate restTemplate() throws Exception {
 
-        if (trustStorePassword == null || trustStorePassword.isEmpty()) {
-            System.out.println("AVISO: trust.store.password não definida. Usando RestTemplate padrão.");
-            return new RestTemplate();
-        }
+        System.out.println("AVISO: Iniciando RestTemplate em MODO INSEGURO (Bypass SSL Ativo).");
 
-        File trustStoreFile = ResourceUtils.getFile(trustStorePath);
+        // Estratégia de confiança que aceita TUDO (Bypass)
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 
         SSLContext sslContext = SSLContextBuilder.create()
-                .loadTrustMaterial(trustStoreFile, trustStorePassword.toCharArray())
+                .loadTrustMaterial(null, acceptingTrustStrategy)
                 .build();
 
-        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext);
+        // Desabilita a verificação de Hostname (Nome do domínio no certificado)
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(
+                sslContext,
+                NoopHostnameVerifier.INSTANCE
+        );
 
         HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
                 .setSSLSocketFactory(sslSocketFactory)
